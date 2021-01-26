94b5245a7647e8ce735b7ea53db172cffe24496d
Anca Toma <ancatoma@revoke.com>
2021-01-26 21:39:29 +0000

    few updates

--- tracker.py
+++ tracker.py
@@ -4,8 +4,10 @@ import time
 # global variables
 api_key = 'bcbe1f6d-dd9b-4c24-8e61-ced8e503b1a1'
 bot_token = '1674988284:AAEQxnx5oIp7y4bxezR56WpXthM9jykTl-c'
-chat_id = '859209026'
-threshold = 40000
+chat_id_anca = '859209026'
+chat_id_alex = '708494963'
+threshold_large = 30000
+threshold_small = 20000
 time_interval = 5 * 60  # in seconds
 
 
@@ -22,16 +24,22 @@ def get_btc_price():
 
     # extract the bitcoin price from the json data
     btc_price = response_json['data'][0]
+    print(btc_price)
     return btc_price['quote']['USD']['price']
 
 
-def send_message(chat_id, msg):
+def send_message_to_Anca(chat_id, msg):
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={msg}"
 
     # send the msg
     requests.get(url)
 
-# main fn
+
+def send_message_to_Alex(chat_id, msg):
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={msg}"
+
+    # send the msg
+    requests.get(url)
 
 
 def main():
@@ -43,12 +51,22 @@ def main():
         price_list.append(price)
 
         # if the price falls below threshold, send an immediate msg
-        if price < threshold:
-            send_message(chat_id=chat_id, msg=f'BTC Price Drop Alert: {price}')
+        if price < threshold_large:
+            send_message_to_Anca(chat_id=chat_id_anca,
+                                 msg=f'BTC Price Drop Alert: {price}')
+            send_message_to_Alex(chat_id=chat_id_alex,
+                                 msg=f'Iuubi, a scazut Bitcoinul, acum e $ {price}')
+
+        if price < threshold_small:
+            send_message_to_Anca(chat_id=chat_id_anca,
+                                 msg=f'BTC Price Drop Alert: {price}')
+            send_message_to_Alex(chat_id=chat_id_alex,
+                                 msg=f'Iuubi, a scazut Bitcoinul, acum e $ {price}')
 
         # send last 6 btc price
         if len(price_list) >= 6:
-            send_message(chat_id=chat_id, msg=price_list)
+            send_message_to_Anca(chat_id=chat_id_anca, msg=price_list)
+            send_message_to_Alex(chat_id=chat_id_alex, msg=price_list)
             # empty the price_list
             price_list = []
 
